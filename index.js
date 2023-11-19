0c7e871439491df169d1c3e80a6a79a92690a169
Rodrigo Vinicius Leandrin <108472050+RodrigoLeandrin@users.noreply.github.com>
2023-11-19 10:28:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 
-alert ("olá") ;
-var Katherine = "Katherine";
-var Person = prompt ("What is your name?");
-if (Person === Katherine) {
-    alert ("Você é meu amor !!!!!")
-    
+alert ("FizzBuzzChallengePleaseCheckConsole") ;
+var output = [];
+var count = 1;
 
+
+function FizzBuzz(){
+    if(count%3===0 && count%5===0){
+        output.push("FizzBuzz");
+ }
+    else if(count%3===0){
+        output.push("Fizz");
+    }
+
+    else if (count%5===0){
+        output.push("Buzz");
+    }
+ else{
+    output.push(count);
+ }
+ count++;
+    alert(output);
 }
+
